456ba67144a3fba5f0455ef2b55074c7fa242873
robbie <robbiecsumner@gmail.com>
2023-08-16 09:28:11 +0200

    add integration tests?

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.18;
 
 import {Test, Vm} from "forge-std/Test.sol";
-import {Raffle} from "../src/Raffle.sol";
-import {DeployRaffle} from "../script/DeployRaffle.s.sol";
-import {HelperConfig} from "../script/HelperConfig.s.sol";
+import {Raffle} from "../../src/Raffle.sol";
+import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
 
 contract RaffleTest is Test {
